9d8a5344945bcb91a429ae666385d17faf1e704e
mathacka <japow75@gmail.com>
2012-09-26 19:31:30 -0400

    first commit

---
+++ ComputerInvaders/conf.lua
@@ -0,0 +1,9 @@
+function love.conf(a)
+	a.title = "Computer Invaders";
+	a.author = "Jay Powers";
+	a.version = "0.1.0";
+	a.console = true;
+	
+	a.screen.width = 1024;
+	a.screen.height = 768;
+end
